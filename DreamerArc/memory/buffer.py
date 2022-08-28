0e7ac26cbcb43da115042303e09c19f149d92eae
Ty <thibault.de.chezelles@gmail.com>
2022-08-28 19:03:45 +0200

    fixed indexing

--- DreamerArc/memory/buffer.py
+++ DreamerArc/memory/buffer.py
@@ -56,13 +56,13 @@ class Buffer:
     def sample(self, batch_size: int, chunk_size: int):
         ep_idx = torch.randint(0, len(self.episodes), (batch_size,))
 
-        ep_max_lengths = [torch.randint(self.ep_lengths[i] - chunk_size - 1, (1,)) for i in ep_idx]
+        ep_max_lengths = [torch.randint(1, self.ep_lengths[i] - chunk_size, (1,)) for i in ep_idx]
 
         obs, act, rew, done = [], [], [], []
         for i, l in zip(ep_idx, ep_max_lengths):
-            obs.append(self.episodes[i].observation[l:l+chunk_size+1])
-            act.append(self.episodes[i].action[l+1:l+chunk_size+1])
-            rew.append(self.episodes[i].reward[l+1:l+chunk_size+1])
-            done.append(self.episodes[i].done[l+1:l+chunk_size+1])
+            obs.append(self.episodes[i].observation[l-1:l+chunk_size])
+            act.append(self.episodes[i].action[l:l+chunk_size])
+            rew.append(self.episodes[i].reward[l:l+chunk_size])
+            done.append(self.episodes[i].done[l:l+chunk_size])
         
         return [torch.stack(i, dim=0) for i in (obs, act, rew, done)]
